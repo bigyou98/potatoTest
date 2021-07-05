d49d329800e1741c48e7350dcddfae88181564ba
DESKTOP-R375PKT\sanghun <bigyou00@gmail.com>
2021-07-05 23:30:04 +0900

    시작 전 세팅

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import "./App.css";
+import React from "react";
+import { MainTodo } from "./todolist";
+import _ from "./App.css";
 
 function App() {
-  return <div>안녕</div>;
+  return (
+    <div>
+      <MainTodo />
+    </div>
+  );
 }
-
 export default App;
--- src/todolist/MainTodo.jsx
+++ src/todolist/MainTodo.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+// 여기가 메인이다.
+// 여기서 컴포넌트들 다 포함시킨후에 app.js로 내보낸다.
 
-const MainTodo = () => {
-  return <div></div>;
+export const MainTodo = () => {
+  const [qwe, setQwe] = useState("qwe");
+  const change = (e) => {
+    console.log(e);
+    console.log(qwe);
+  };
+  return (
+    <SmartPhone>
+      <Header>To Do List</Header>
+    </SmartPhone>
+  );
 };
-
-export default MainTodo;
+const SmartPhone = styled.div`
+  text-align: center;
+  border-radius: 64px;
+  width: 640px;
+  height: 1000px;
+  margin: auto;
+  background-color: #dfd9d9;
+  box-shadow: 29px 23px 18px -6px rgba(197, 197, 197, 1);
+  -webkit-box-shadow: 29px 23px 18px -6px rgba(197, 197, 197, 1);
+  -moz-box-shadow: 29px 23px 18px -6px rgba(197, 197, 197, 1);
+`;
+const Header = styled.div`
+  background: rgb(57, 57, 55);
+  background: linear-gradient(
+    8deg,
+    rgba(57, 57, 55, 1) 0%,
+    rgba(241, 241, 241, 1) 11%,
+    rgba(31, 31, 31, 0.8239670868347339) 49%,
+    rgba(248, 245, 249, 1) 91%
+  );
+  padding: 20px;
+  font-size: 24px;
+  font-weight: bold;
+  border-radius: 64px 64px 0 0;
+  margin-top: 20px;
+`;
